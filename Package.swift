86aadd769666f5519d85ad71fe9fba4564f9f941
Carlos Duclos <darkzeratul64@gmail.com>
2018-07-30 01:25:38 -0500

    fix

--- Package.swift
+++ Package.swift
@@ -4,5 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGD",
+    targets: [
+        Target(
+            name: "SwiftGD"
+        )
+    ],
     dependencies: [.Package(url: "https://github.com/carlos21/Cgd.git", "0.0.3")]
 )
--- Sources/SwiftGD.swift
+++ Sources/SwiftGD.swift
@@ -1,4 +1,4 @@
-#if canImport(Darwin)
+#if os(macOS)
 import Darwin
 import Cgdmac
 #else
