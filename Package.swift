924a6c9754d593f57d9017e32fe88a6fda26ab07
Carlos Duclos <darkzeratul64@gmail.com>
2018-07-30 01:19:30 -0500

    first commit

---
+++ Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftGD",
+    dependencies: [.Package(url: "https://github.com/carlos21/Cgd.git", "0.0.3")]
+)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import SwiftGDTests
+
+XCTMain([
+    testCase(SwiftGDTests.allTests),
+])
